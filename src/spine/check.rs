09f89571b3400608611d7521f238b7dfeb1d16ec
Jan Špaček <patek.mail@gmail.com>
2015-03-08 14:18:29 +0100

    Write tests for spine well-formedness checker

--- src/spine/check.rs
+++ src/spine/check.rs
@@ -1,4 +1,5 @@
 use spine;
+use std::collections::{HashSet};
 
 type Env = spine::env::Env<(), usize, usize>;
 
@@ -10,8 +11,12 @@ pub fn check(prog: &spine::ProgDef) -> Vec<String> {
   let fun_env = empty_env.bind_funs(fun_binds);
 
   let mut errors = Vec::new();
+  let mut fun_names = HashSet::new();
   for fun_def in prog.fun_defs.iter() {
     errors.extend(check_fun_def(&fun_env, fun_def).into_iter());
+    if !fun_names.insert(&fun_def.name) {
+      errors.push(format!("redefined function: '{}'", fun_def.name.0));
+    }
   }
 
   if fun_env.lookup_fun(&prog.main_fun).is_none() {
@@ -147,7 +152,6 @@ fn check_boolval(env: &Env, boolval: &spine::Boolval) -> Vec<String> {
 mod test {
   use sexpr;
   use spine;
-  use spine::helpers::*;
 
   fn parse_check(txt: &str) -> Vec<String> {
     let sexpr = sexpr::parse::parse_sexpr(txt).unwrap();
@@ -156,11 +160,76 @@ mod test {
   }
 
   #[test]
-  fn test_undefined_var() {
-    let errors = parse_check("(program main (main halt () (cont halt b)))");
-    assert_eq!(errors.len(), 1);
-    assert!(errors[0].contains("undefined var: 'b'"));
+  fn test_missing_main() {
+    let errs = parse_check("(program question (answer r () (cont r 42)))");
+    assert_eq!(errs.len(), 1);
+    assert!(errs[0].contains("undefined main function: 'question'"));
   }
 
-  // TODO :-)
+  #[test]
+  fn test_double_fun_defs() {
+    let errs = parse_check("(program main 
+      (main k () (cont k 10))
+      (main k () (cont k 20)))");
+    assert_eq!(errs.len(), 1);
+    assert!(errs[0].contains("redefined function: 'main'"));
+  }
+
+  #[test]
+  fn test_undefined_var() {
+    let errs = parse_check("(program main (main halt () (cont halt x)))");
+    assert_eq!(errs.len(), 1);
+    assert!(errs[0].contains("undefined var: 'x'"));
+  }
+
+  #[test]
+  fn test_undefined_conts() {
+    let errs = parse_check(
+      "(program main 
+        (fun r (a b) (cont r 100))
+        (main r () 
+          (letcont ((bad-call () (call fun cc1 1 2))
+                    (bad-ext-call () (extern-call ext_add cc2 1 2))
+                    (bad-cont () (cont cc3 3))
+                    (bad-branch () (branch (is-true 1) cc4 cc5)))
+            (cont r 5))))");
+
+    assert_eq!(errs.len(), 5);
+    assert!(errs[0].contains("return to undefined cont: 'cc1'"));
+    assert!(errs[1].contains("extern return to undefined cont: 'cc2'"));
+    assert!(errs[2].contains("continue to undefined cont: 'cc3'"));
+    assert!(errs[3].contains("then-branch to undefined cont: 'cc4'"));
+    assert!(errs[4].contains("else-branch to undefined cont: 'cc5'"));
+  }
+
+  #[test]
+  fn test_cont_args_mismatch() {
+    let errs = parse_check(
+      "(program main
+        (main r () (cont r 0))
+        (return-42 r () (cont r 42))
+        (bad-return-2 ret (a b) (cont ret a b))
+        (bad-return-0 ret (a b) (cont ret))
+        (bad-call r () 
+          (letcont ((needs-two-args (x y) (cont r x)))
+            (call return-42 needs-two-args)))
+        (bad-extern-call r ()
+          (letcont ((needs-zero-args () (cont r 10)))
+            (extern-call __boo__ needs-zero-args 1 2 3)))
+        (bad-branch r ()
+          (letcont ((needs-one-arg (x) (cont r x))
+                    (needs-two-args (x y) (cont r y)))
+            (branch (is-true 0) needs-one-arg needs-two-args))))");
+
+    assert_eq!(errs.len(),  6);
+    assert!(errs[0].contains("continue args mismatch: 'ret' expects 1 args"));
+    assert!(errs[1].contains("continue args mismatch: 'ret' expects 1 args"));
+    assert!(errs[2].contains("return args mismatch: 'needs-two-args' expects 2 args"));
+    assert!(errs[3].contains("extern return args mismatch: \
+      'needs-zero-args' expects 0 args"));
+    assert!(errs[4].contains("then-branch args mismatch: \
+      'needs-one-arg' expects 1 args"));
+    assert!(errs[5].contains("else-branch args mismatch: \
+      'needs-two-args' expects 2 args"));
+  }
 }
